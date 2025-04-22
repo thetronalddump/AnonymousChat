63381114b7fea7dab735444aaca351ea5deb8824
mbchl <mdev4work@gmail.com>
2025-04-22 19:01:50 +0300

    added visual part of login component

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,11 +1,52 @@
-<script setup>
-
-</script>
-
 <template>
+  <div>
 
+  <el-form :model="form" label-width="auto" style="max-width: 400px">
+    <h1>Login</h1>
+    <el-form-item label="Nickname">
+      <el-input v-model="form.name" />
+    </el-form-item>
+    <el-form-item label="Date of birth">
+      <el-col :span="11">
+        <el-date-picker
+            v-model="form.date_of_birth"
+            type="date"
+            placeholder="Pick a date"
+            style="width: 150px"
+        />
+      </el-col>
+    </el-form-item>
+    <el-form-item label="Gender">
+      <el-radio-group v-model="form.resource">
+        <el-radio value="Male">Male</el-radio>
+        <el-radio value="Female">Female</el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit" style="">Create</el-button>
+    </el-form-item>
+  </el-form>
+  </div>
 </template>
 
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+
+const form = reactive({
+  name: '',
+  date_of_birth: '',
+  gender: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
+
 <style scoped>
+  h1 {
+    text-align: center;
+  }
 
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,11 @@
 
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from 'vuetify'
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './assets/styles.css'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
