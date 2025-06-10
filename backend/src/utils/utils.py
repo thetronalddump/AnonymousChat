0732192650f63f3992fce187462c99374383d806
mbchl <mdev4work@gmail.com>
2025-06-10 17:49:51 +0300

    saving changes

---
+++ backend/src/utils/utils.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from src.models.models import UserModel, RoomModel
+
+
+def find_best_room(current_user: UserModel, rooms: List[RoomModel]):
+    best_match = None
+    min_age_diff = float('inf')
+
+    for room in rooms:
+        if room.status == "waiting":
+            user = room.participants[0]
+            if user.gender != current_user.gender:
+                age_diff = abs(user.age - current_user.age)
+                if age_diff < min_age_diff:
+                    min_age_diff = age_diff
+                    best_match = room
+
+    return best_match if best_match else False
