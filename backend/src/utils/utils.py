a5f10d2be3575ccb55d8f582d3030ad15c40a5bb
mbchl <mdev4work@gmail.com>
2025-06-15 20:22:46 +0300

    Added distribution of users by rooms

--- backend/src/utils/utils.py
+++ backend/src/utils/utils.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 
 from src.models.models import UserModel, RoomModel
@@ -6,14 +7,15 @@ from src.models.models import UserModel, RoomModel
 def find_best_room(current_user: UserModel, rooms: List[RoomModel]):
     best_match = None
     min_age_diff = float('inf')
-
-    for room in rooms:
-        if room.status == "waiting":
-            user = room.participants[0]
-            if user.gender != current_user.gender:
-                age_diff = abs(user.age - current_user.age)
-                if age_diff < min_age_diff:
-                    min_age_diff = age_diff
-                    best_match = room
+    if rooms:
+        for room in rooms:
+            room = RoomModel.model_validate(json.loads(room[0]))
+            if room.status == "waiting":
+                user = room.participants[0]
+                if user.gender != current_user.gender:
+                    age_diff = abs(user.age - current_user.age)
+                    if age_diff < min_age_diff:
+                        min_age_diff = age_diff
+                        best_match = room
 
     return best_match if best_match else False
