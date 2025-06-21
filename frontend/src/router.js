740f837eacc36ed46a73c40c66738719f0da588a
mbchl <mdev4work@gmail.com>
2025-06-21 23:28:18 +0300

    added main page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from './components/Login.vue'
 import Chat from './components/Chat.vue'
+import Main from "@/components/Main.vue";
 
 const routes = [
     { path: '/login', component: Login, name: 'Login' },
     { path: '/chat', component: Chat, name: 'Chat' },
+    { path: '/', component: Main, name: 'Main' },
 ]
 
 export const router = createRouter({
